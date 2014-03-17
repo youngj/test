3aa571d823b16710765a09a4f2c3de557a938249
Jesse Young <adunar@gmail.com>
2014-03-16 20:51:51 -0700

    Update quux.js

--- quux.js
+++ quux.js
@@ -13,5 +13,15 @@ console.log(httpClient.request("http://www.google.com").content.length);
 console.log("content = " + content);
 console.log("from_number = " + message.from_number);
 
+var dom = new DOMImplementation();
+var doc2 = dom.createDocument();
+var foo = doc2.createElement('foo');
+foo.setAttribute('baz', '1213');
+var bar = doc2.createElement('bar');
+bar.appendChild(doc2.createTextNode('wtf'));
+doc2.appendChild(foo);
+foo.appendChild(bar);
+
+console.log('' + doc2);
 
 //console.log("word1 = " + word1);
